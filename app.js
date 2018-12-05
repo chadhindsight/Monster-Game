d6ecac618d63548b38056ae956c009ffbce59f06
Chadrick Hinds <chadrickj8@gmail.com>
2018-12-05 09:29:00 -0500

    Change first arg in damage function to 11

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function () {
-            var damage = this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(11, 20);
             this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
